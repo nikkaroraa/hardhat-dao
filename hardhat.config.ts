5b4180300510cdf92b54c3348bd4fd69c283c9b5
nikhil arora <narora200@gmail.com>
2022-08-05 13:50:55 +0530

    chore: deployment scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -48,7 +48,7 @@ const config: HardhatUserConfig = {
     currency: "USD",
     outputFile: "gas-report.txt",
     noColors: true,
-    // coinmarketcap: COINMARKETCAP_API_KEY,
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
   namedAccounts: {
     deployer: {
