86097d4390c416dec9fafbcf811f03f98f2ab400
nikhil arora <narora200@gmail.com>
2022-08-05 22:27:37 +0530

    chore: scripts to demo functionality

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -23,3 +23,8 @@ export const MIN_DELAY = 3600 // 1 hour - after a vote passes, you have 1 hour b
 export const VOTING_PERIOD = 5 // blocks
 export const VOTING_DELAY = 1 // 1 block - how many blocks till a proposal vote becomes active
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
+
+export const proposalsFile = "proposals.json"
+export const NEW_STORE_VALUE = 77
+export const FUNC = "store"
+export const PROPOSAL_DESCRIPTION = "Proposal #1 77 in the Box!"
